438ff29072ea71a4621b63102f0b7cb202ebd3f3
Mina Sameh <minasamehlabib@gmail.com>
2021-04-08 17:49:37 +0200

    Initial Commit

---
+++ VIPER-codegen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIPER-codegen
+//
+//  Created by Mina Sameh on 8/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
